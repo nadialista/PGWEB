3d8a3b16a8964508de52d38d4398308450b41ea7
nadialista <birgittanadialista@gmail.com>
2023-11-14 14:45:22 +0700

    Example for PGWEB2

---
+++ data/wfsgeojson1.php
@@ -0,0 +1,9 @@
+<?php
+	//$wfsUrl = file_get_contents("http://geoportal.slemankab.go.id/geoserver/geonode/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geonode:analisis_resiko_bencana_gunung_api&outputFormat=json");
+
+	$wfsUrl = file_get_contents("http://localhost:8080/geoserver/ACARA_IV/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ACARA_IV%3AFAS_KESEHATAN_PT_SLEMAN_RSRUJUKAN_Proj&outputFormat=application%2Fjson");
+
+	header('Content-type: application/json');
+	echo ($wfsUrl);
+	# Jika terdapat &maxFeatures=50 pada url wfs geojson, dihapus supaya jumlah feature tidak dibatasi
+?>
